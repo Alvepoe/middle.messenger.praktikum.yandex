4d8cb80911a8399d14f5fbcae909c65deeef8468
alvepoe <alve1507@yandex.ru>
2021-11-17 09:45:13 +0300

    feat: registered partial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import template from './components/link/link.hbs'
-import './components/link/link.scss'
+import "./components/field/field";
+import template from "./index.hbs";
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.body.innerHTML = template({link: {linkUrl: '#', linkText: 'Link'}})
-})
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.innerHTML = template({
+    field: {
+      type: "text",
+      name: "name",
+      label: "label",
+      placeholder: "placeholder",
+      error: "error",
+    },
+  });
+});
