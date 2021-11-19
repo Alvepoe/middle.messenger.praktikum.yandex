9a3a483873276577654c15e6cc9af6c7fcf4eaa2
alvepoe <alve1507@yandex.ru>
2021-11-19 09:52:53 +0300

    refactor: added error component

--- src/modules/page/page.js
+++ src/modules/page/page.js
@@ -1,5 +1,6 @@
 import Handlebars from "handlebars/dist/handlebars.runtime";
 import '../../modules/form/form'
+import '../../components/error/error'
 import template from "./page.hbs";
 
 Handlebars.registerPartial("page", template);
