f5a282117e790283ac78423a2c2895d51df5c569
alvepoe <alve1507@yandex.ru>
2021-11-21 15:53:50 +0300

    fix: another attempt to pass test

--- server.js
+++ server.js
@@ -6,8 +6,5 @@ const PORT = 3000;
 const server = express()
 
 server.use('/', express.static(path.join(__dirname, 'dist')))
-server.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
-})
 
 server.listen(PORT, () => console.log(`Сервер на ${PORT} порту`))
