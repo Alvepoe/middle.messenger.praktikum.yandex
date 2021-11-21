1bc07b70d7b7b43f4a3e0185a909ca66bfa8f78c
alvepoe <alve1507@yandex.ru>
2021-11-21 11:03:44 +0300

    feat: change password page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './pages/404/404'
 import './pages/500/500'
 import './modules/profile/profile'
 import './pages/profileSettings/profileSettings'
+import './pages/changePassword/changePassword'
 import './components/profileField/profileField'
 import template from "./index.hbs";
 
---
+++ src/pages/changePassword/changePassword.js
@@ -0,0 +1,5 @@
+import Handlebars from "handlebars/dist/handlebars.runtime";
+import '../../helpers/getConfigHelper'
+import template from "./changePassword.hbs";
+
+Handlebars.registerPartial("changePassword", template);
