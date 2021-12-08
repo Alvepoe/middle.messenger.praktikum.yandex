41431788d25105482ad0e7e77002e49e80aa008b
alvepoe <alve1507@yandex.ru>
2021-12-08 16:03:53 +0300

    feat: chat preview component

---
+++ src/components/chatPreview/chatPreview.ts
@@ -0,0 +1,5 @@
+import Handlebars from 'handlebars/dist/handlebars.runtime';
+import template from './chatPreview.hbs';
+import './chatPreview.scss';
+
+Handlebars.registerPartial('chatPreview', template);
